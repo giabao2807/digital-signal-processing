87cb453e150d40e3b8d97485779904790f8e0d15
GiaBaone <102190252@sv1.dut.udn.vn>
2021-12-07 20:26:11 +0700

    edit divided frame ex02 7.12.21

--- endcourse/bai2.m
+++ endcourse/bai2.m
@@ -9,6 +9,8 @@ name = char(strcat(filePath, files(looping), '.wav'));
     [x,t1,fs]=normalizedAmplitude(name);
 
     N=32768;
+    %gioi han tan so F0 trong khoang 70hz->450hz->giam do phuc tap
+    
     time_duration=0.03; %do dai moi khung 
     lag = 0.01; %do tre moi khung
     pdlow=1/450; pdhigh=1/70; 
@@ -18,31 +20,32 @@ name = char(strcat(filePath, files(looping), '.wav'));
     n_max = floor(pdhigh*fs);
 
     lenX = length(x); %do dai tin hieu vao theo mau
-    nSampleFrame = time_duration*fs; %do dai 1 frame tinh theo mau
-    h=hamming(nSampleFrame); 
+    nSampleFrame = time_duration*fs;%do dai 1 frame tinh theo mau
+     h=hamming(nSampleFrame); 
     nSampleLag = lag*fs; %do dai do dich cua frame theo mau
-    sec=length(x)/fs;
+
     
-    nFrame= int32(lenX-nSampleFrame)/nSampleLag+1; %so frame chia duoc
-    t=(1:nFrame)*fs/nSampleLag; %truc thgian
+    nFrame= int32((lenX-nSampleLag)/(nSampleFrame-nSampleLag))+1;%so frame chia duoc
+    t=(1:nFrame)*2*fs/nSampleLag; 
     
     F0_frame=zeros(1,nFrame);
-    
-    %normalized
-    max_value = max(abs(x)); %tim bien do lon nhat cua tin hieu duoc doc
-    x= x/max_value; %dua bien do cua tin hieu ve vecto co tin hieu cao nhat 
     
     
     %chia frame
    for frame_index=1:nFrame
-        a=(frame_index-1)*nSampleLag+1;
-        b=(frame_index-1)*nSampleLag+nSampleFrame+1;
-        if b <= lenX
+        a=(frame_index-1)*(nSampleFrame-nSampleLag)+1;
+        if(frame_index ==1)
+             b=(frame_index)*nSampleFrame+1;
+        else
+            b=(frame_index)*nSampleFrame - (frame_index-1)*nSampleLag +1;
+        end
+        if b < lenX
             frame= x(a:b); %xac dinh 1 frame
         else 
             frame= x(a:lenX);
             frame(lenX:b)=0;
         end
+        
         
        
         
@@ -69,14 +72,14 @@ name = char(strcat(filePath, files(looping), '.wav'));
             end
         end
         
-        %kho?ng c?ch gi?a c?c h?i
-       F0frame = zeros(1:3);
-       for j=1:3
-           F0frame = xxN(framepeak(j+1))/framepeak(j+1)-xxN(framepeak(j))/framepeak(j);
-       end
+      %khoachcach
+%        F0frame = zeros(1:3);
+%        for j=1:3
+%            F0frame = xxN(framepeak(j+1))/framepeak(j+1)-xxN(framepeak(j))/framepeak(j);
+%        end
        
        %T0 chu k?
-       F0_frame(frame_index) = sum(F0frame)/3;
+      % F0_frame(frame_index) = sum(F0frame)/3;
     end
      
     figure;
